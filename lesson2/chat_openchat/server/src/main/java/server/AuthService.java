4296eb6d7d4c90ec01c10729e30fea20e6529b9e
Nastushepulechka <nastya..voronin2010@yandex.ru>
2021-05-13 23:07:20 +0300

    дз2

---
+++ lesson2/chat_openchat/server/src/main/java/server/AuthService.java
@@ -0,0 +1,18 @@
+package server;
+
+public interface AuthService {
+    /**
+     * Метод получения никнейма по логину и паролю.
+     * Если учетки с таким логином и паролем нет то вернет
+     * Если учетка есть то вернет никнейм.
+     * @return никнейм если есть совпадение по логину и паролю, null если нет совпадения
+     * */
+    String getNicknameByLoginAndPassword(String login, String password );
+
+    /**
+     * Попытка регистрации новой учетной записи
+     * */
+    boolean registration(String login, String password, String nickname);
+
+    boolean changeNickname(String oldNickname, String newNickname);
+}
